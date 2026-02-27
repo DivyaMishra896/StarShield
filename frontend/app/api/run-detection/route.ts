69b884b98f69df656ac19dac61d8ec09f1d39c68
DivyaMishra896 <divya2006mishra@gmail.com>
2026-02-27 17:48:19 +0530

    Update route.ts

--- frontend/app/api/run-detection/route.ts
+++ frontend/app/api/run-detection/route.ts
@@ -1,11 +1,16 @@
 import { NextResponse } from "next/server";
 
 export async function POST() {
-  const res = await fetch("http://localhost:8000/run-detection", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-  });
+const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000";
 
-  const data = await res.json();
-  return NextResponse.json(data);
+const res = await fetch(${backendUrl}/run-detection, {
+method: "POST",
+headers: {
+"Content-Type": "application/json",
+"ngrok-skip-browser-warning": "true"
+},
+});
+
+const data = await res.json();
+return NextResponse.json(data);
 }
