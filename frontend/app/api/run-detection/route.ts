77de4084601024b94e0879e5db298e0660e9ef68
DivyaMishra896 <divya2006mishra@gmail.com>
2026-02-28 13:20:56 +0530

    Update route.ts

--- frontend/app/api/run-detection/route.ts
+++ frontend/app/api/run-detection/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function POST() {
-const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "https://starshield.onrender.com/";
+const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000";
 
 const res = await fetch(${backendUrl}/run-detection, {
 method: "POST",
